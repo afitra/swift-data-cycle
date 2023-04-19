26bbcd15556d179261af9a07f2dd1174791b7462
afitra <loeby45@gmail.com>
2023-04-19 16:19:42 +0700

    Initial Commit

---
+++ swift-data-cycle/swift_data_cycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_data_cycleApp.swift
+//  swift-data-cycle
+//
+//  Created by afitra mamor on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_data_cycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
